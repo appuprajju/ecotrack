13d04b8ea3b8224a52f30397c940abc2af214e0a
appuprajju <appuprajju807@gmail.com>
2026-06-21 00:07:23 +0530

    fix(backend): resolve deployment port conflict and cloudsql connection in CI/CD

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,9 +12,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-})
 
 // Security and utility Middlewares
 app.use(helmet());
